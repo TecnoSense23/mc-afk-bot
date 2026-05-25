b0f9a01d89b086db7fa97d67836aaec8840adcbc
TecnoSense23 <gamerprinceytjethwa@gmail.com>
2026-05-25 12:19:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function createBot() {
         auth: 'offline',
         
         // The magic fix for your specific server
-        version: '1.12.1' 
+        version: '1.20.4' 
     });
 
     bot.on('login', () => console.log('✅ Bot successfully joined the server!'));
